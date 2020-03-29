73a52e778b408c617250fb037e89f839713227da
Necas209 <58083031+Necas209@users.noreply.github.com>
2020-03-29 20:46:00 +0100

    ....

--- Sopa de letras/Sopa de letras/Tabuleiro.cpp
+++ Sopa de letras/Sopa de letras/Tabuleiro.cpp
@@ -16,18 +16,13 @@ Tabuleiro::~Tabuleiro()
 
 void Tabuleiro::Create_matriz()
 {
-	char l;
-	vector<vector<Letra>> matriz;
+	char l = ' ';
 	matriz.resize(DimY, vector<Letra>(DimX));
 	for (int i = 0; i < DimY; i++) 
 	{
 		for (int j = 0; j < DimX; j++) 
-		{
-			l = ' ';
 			matriz[i][j] = Letra(l, Ponto(j, i), l, 0);
-		}
 	}
-	Set_matriz(matriz);
 }
 
 void Tabuleiro::Fill_matriz(void)
@@ -62,12 +57,18 @@ void Tabuleiro::Show_matriz()
 	}
 }
 
-void Tabuleiro::Ask_DimX() {
+void Tabuleiro::Ask_DimX() 
+{
 	int dim_x;
-	cout << endl << "PS: Recomenda-se a escolha de dimensões mínimas de 17*17" << endl;
 	cout << endl << "Insira a dimensão X da matriz: ";
 	cin >> dim_x;
-	Set_DimX(dim_x);
+	if (dim_x <= 0)
+	{
+		cout << " Dimensão inválida." << endl;
+		Ask_DimX();
+	}
+	else
+		Set_DimX(dim_x);
 }
 
 void Tabuleiro::Ask_DimY()
@@ -75,7 +76,13 @@ void Tabuleiro::Ask_DimY()
 	int dim_y;
 	cout << endl << "Insira a dimensão Y da matriz: ";
 	cin >> dim_y;
-	Set_DimY(dim_y);
+	if (dim_y <= 0)
+	{
+		cout << " Dimensão inválida." << endl;
+		Ask_DimY();
+	}
+	else
+		Set_DimY(dim_y);
 }
 
 void Tabuleiro::Load_lista()
@@ -108,40 +115,40 @@ bool Tabuleiro::CheckIfItFits(int i)
 {
 	int x = lista[i].Get_ponto_inicial().Get_x();
 	int y = lista[i].Get_ponto_inicial().Get_y();
-	int c = lista[i].Get_palavra().size();
+	int l = lista[i].Get_palavra().size();
 	int o = lista[i].Get_orientacao();
 	switch (o)
 	{
 	case 1:
-		if ((x + c) > DimX)
+		if ((x + l) > DimX)
 			return 0;
 		break;
 	case 2:
-		if ((x + 1 - c) < 0)
+		if ((x + 1 - l) < 0)
 			return 0;
 		break;
 	case 3:
-		if ((y + c) > DimY)
+		if ((y + l) > DimY)
 			return 0;
 		break;
 	case 4:
-		if ((y + 1 - c) < 0)
+		if ((y + 1 - l) < 0)
 			return 0;
 		break;
 	case 5:
-		if ((x + c) > DimX or (y + c) > DimY)
+		if ((x + l) > DimX or (y + l) > DimY)
 			return 0;
 		break;
 	case 6:
-		if ((x + 1 - c) < 0 or (y + 1 - c) < 0)
+		if ((x + 1 - l) < 0 or (y + 1 - l) < 0)
 			return 0;
 		break;
 	case 7:
-		if ((x + 1 - c) < 0 or (y + c) > DimY)
+		if ((x + 1 - l) < 0 or (y + l) > DimY)
 			return 0;
 		break;
 	case 8:
-		if ((x + c) > DimX or (y + 1 - c) < 0)
+		if ((x + l) > DimX or (y + 1 - l) < 0)
 			return 0;
 		break;
 	}
@@ -152,56 +159,56 @@ bool Tabuleiro::CheckCrossing(int i)
 {
 	int x = lista[i].Get_ponto_inicial().Get_x();
 	int y = lista[i].Get_ponto_inicial().Get_y();
-	int c = lista[i].Get_palavra().size();
+	int l = lista[i].Get_palavra().size();
 	int o = lista[i].Get_orientacao();
 	if (i == 0)
 		return 1;
 	switch (o) 
 	{
 	case 1:
-		for (int j = x; j < (x + c); j++) {
+		for (int j = x; j < (x + l); j++) {
 			if (!CheckLetter(i, j, y, j-x))
 				return false;
 		}
 		break;
 	case 2:
-		for (int j = x; j > (x - c); j--) {
+		for (int j = x; j > (x - l); j--) {
 			if (!CheckLetter(i, j, y, x-j))
 				return false;
 		}
 		break;
 	case 3:
-		for (int j = y; j < (y + c); j++) {
+		for (int j = y; j < (y + l); j++) {
 			if (!CheckLetter(i, x, j, j-y))
 				return false;
 		}
 		break;
 	case 4:
-		for (int j = y; j > (y - c); j--) {
+		for (int j = y; j > (y - l); j--) {
 			if (!CheckLetter(i, x, j, y-j))
 				return false;
 		}
 		break;
 	case 5:
-		for (int j1 = x, j2 = y; j1 < (x + c) and j2 < (y + c); j1++, j2++) {
+		for (int j1 = x, j2 = y; j1 < (x + l) and j2 < (y + l); j1++, j2++) {
 			if (!CheckLetter(i, j1, j2, j1-x))
 				return false;
 		}
 		break;
 	case 6:
-		for (int j1 = x, j2 = y; j1 > (x - c) and j2 > (y - c); j1--, j2--) {
+		for (int j1 = x, j2 = y; j1 > (x - l) and j2 > (y - l); j1--, j2--) {
 			if (!CheckLetter(i, j1, j2, x-j1))
 				return false;
 		}
 		break;
 	case 7:
-		for (int j1 = x, j2 = y; j1 > (x - c) and j2 < (y + c); j1--, j2++) {
+		for (int j1 = x, j2 = y; j1 > (x - l) and j2 < (y + l); j1--, j2++) {
 			if (!CheckLetter(i, j1, j2, x-j1))
 				return false;
 		}
 		break;
 	case 8:
-		for (int j1 = x, j2 = y; j1 < (x + c) and j2 > (y - c); j1++, j2--) {
+		for (int j1 = x, j2 = y; j1 < (x + l) and j2 > (y - l); j1++, j2--) {
 			if (!CheckLetter(i, j1, j2, j1-x))
 				return false;
 		}
@@ -221,40 +228,40 @@ void Tabuleiro::Insert_palavra(int i)
 {
 	int x = lista[i].Get_ponto_inicial().Get_x();
 	int y = lista[i].Get_ponto_inicial().Get_y();
-	int c = lista[i].Get_palavra().size();
+	int l = lista[i].Get_palavra().size();
 	int o = lista[i].Get_orientacao();
 	switch (o)
 	{
 	case 1:
-		for (int j = x; j < (x + c); j++)
+		for (int j = x; j < (x + l); j++)
 			matriz[y][j] = lista[i].Get_palavra()[j - x];
 		break;
 	case 2:
-		for (int j = x, k = 0; j > (x - c) and k < c; j--, k++)
+		for (int j = x, k = 0; j > (x - l) and k < l; j--, k++)
 			matriz[y][j] = lista[i].Get_palavra()[k];
 		break;
 	case 3:
-		for (int j = y; j < (y + c); j++)
+		for (int j = y; j < (y + l); j++)
 			matriz[j][x] = lista[i].Get_palavra()[j - y];
 		break;
 	case 4:
-		for (int j = y, k = 0; j > (y - c) and k < c; j--, k++)
+		for (int j = y, k = 0; j > (y - l) and k < l; j--, k++)
 			matriz[j][x] = lista[i].Get_palavra()[k];
 		break;
 	case 5:
-		for (int j1 = x, j2 = y; j1 < (x + c) and j2 < (y + c); j1++, j2++)
+		for (int j1 = x, j2 = y; j1 < (x + l) and j2 < (y + l); j1++, j2++)
 			matriz[j2][j1] = lista[i].Get_palavra()[j1 - x];
 		break;
 	case 6:
-		for (int j1 = x, j2 = y, k = 0; j1 > (x - c) and j2 > (y - c) and k < c; j1--, j2--, k++)
+		for (int j1 = x, j2 = y, k = 0; j1 > (x - l) and j2 > (y - l) and k < l; j1--, j2--, k++)
 			matriz[j2][j1] = lista[i].Get_palavra()[k];
 		break;
 	case 7:
-		for (int j1 = x, j2 = y; j1 > (x - c) and j2 < (y + c); j1--, j2++)
+		for (int j1 = x, j2 = y; j1 > (x - l) and j2 < (y + l); j1--, j2++)
 			matriz[j2][j1] = lista[i].Get_palavra()[j2 - y];
 		break;
 	case 8:
-		for (int j1 = x, j2 = y; j1 < (x + c) and j2 > (y - c); j1++, j2--)
+		for (int j1 = x, j2 = y; j1 < (x + l) and j2 > (y - l); j1++, j2--)
 			matriz[j2][j1] = lista[i].Get_palavra()[j1 - x];
 		break;
 	}
